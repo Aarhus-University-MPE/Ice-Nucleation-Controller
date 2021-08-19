87edf035433d53d48bc7ce9df64f1021ea3fc370
MadsR <madsrosenhoej@gmail.com>
2021-08-19 16:26:50 +0200

    Data Calibration

--- src/Python/Calibration.py
+++ src/Python/Calibration.py
@@ -8,7 +8,7 @@ import pandas as pd
 import os
 
 #%%
-sensorList = [0, 4, 5, 6, 7]
+sensorList = [5, 6, 7]
 folder = 'C:\\Users\\au540322\\Documents\\Projects\\Ice-Nucleation\\Builds\\Full Build\\Data\\'
 sub_folders = [name for name in os.listdir(folder) if os.path.isdir(os.path.join(folder, name))]
 
@@ -30,6 +30,16 @@ for sample in sub_folders:
         pass
     msft['Time [MM:SS]']=pd.to_datetime(msft['Time [MM:SS]'],format='%H-%M-%S_%f').dt.strftime('%H:%M')
 
+    axes = msft.plot(0,['V1','V5','V6','V7','V8'],figsize = (30,10), grid = True, fontsize = 25,
+    sharey=True, lw = 1)
+
+    axes.set_title(str(sample),fontsize = 30)
+    axes.set_ylabel('Measured Voltage [V]', fontsize = 25)
+    axes.set_xlabel('Time [HH:mm]', fontsize = 25)
+
+    axes.legend(fontsize=30,bbox_to_anchor=(1.01, 1))
+    axes.minorticks_on()
+    axes.grid(True,which='minor',axis='x')
     
     for i in sensorList:
         calibrationValue = int(float(setupData.iloc[9,1].replace(',', '.')))
@@ -63,12 +73,12 @@ for i in range(1):
     print(")")
 
 #%%
-sample = 'Calibration_1'
+sample = 'Calibration_30'
 file_location = folder + sample + '\\Data.csv'
 msft = pd.read_csv(file_location,delimiter=';',decimal=',')
 msft['Time [MM:SS]']=pd.to_datetime(msft['Time [MM:SS]'],format='%H-%M-%S_%f').dt.strftime('%H:%M')
 
-axes = msft.plot(0,['V1','V2','V5','V6','V7','V8'],figsize = (30,10), grid = True, fontsize = 25,
+axes = msft.plot(0,['V6','V8'],figsize = (30,10), grid = True, fontsize = 25,
     sharey=True, lw = 1)
 
 axes.set_title(str(sample),fontsize = 30)
